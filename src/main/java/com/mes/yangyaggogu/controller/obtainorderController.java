92a98c57edbcc9211d8163d9190d53c602fd3662
김진영 <154316245+k-jinyoung@users.noreply.github.com>
2024-06-14 22:48:28 +0900

    20240614 수주등록

--- src/main/java/com/mes/yangyaggogu/controller/obtainorderController.java
+++ src/main/java/com/mes/yangyaggogu/controller/obtainorderController.java
@@ -19,12 +19,18 @@ public class obtainorderController {
     }
 
     //수주현황 조회
-    @GetMapping("orderstatus")
+    @GetMapping("/orderstatus")
     public void orderStatus(long id, Model model){
         System.out.println("id: " + id);
 
         OrderStateDto orderStateDto = obtainorderService.read(id);
 
         model.addAttribute("dto", orderStateDto);
+    }
+
+    //수주 등록 페이지 띄우기(임시)
+    @GetMapping("/order_reg")
+    public String orderReg(){
+        return "obtainorder/OrderRegistration";
     }
 }
