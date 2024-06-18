6ecab4486cc2b53190bb15e3e68e5a4c110da150
hyeong seok <qnftlstm@naver.com>
2024-06-18 17:20:59 +0900

    20240618
    20240618

--- src/main/java/com/mes/yangyaggogu/repository/companyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mes.yangyaggogu.repository;
-
-import com.mes.yangyaggogu.entity.company;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface companyRepository extends JpaRepository<company, String> {
-}
