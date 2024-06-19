a2cca6de71b7e4a8903c50b90ef2bca336b92813
hyeong seok <qnftlstm@naver.com>
2024-06-19 11:46:38 +0900

    20240619

--- src/main/java/com/mes/yangyaggogu/dto/P_plan_calendarDTO.java
+++ src/main/java/com/mes/yangyaggogu/dto/P_plan_calendarDTO.java
@@ -7,8 +7,9 @@ import java.time.LocalDate;
 @Data
 public class P_plan_calendarDTO {
 
-    private String start;
-    private String end;
+    private LocalDate start;
+    private LocalDate end;
     private String title;
+    private String color;
 
 }
