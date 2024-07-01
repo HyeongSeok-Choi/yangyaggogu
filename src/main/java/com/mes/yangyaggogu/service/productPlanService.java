568fbf74201799d6878005862f58d147405f255b
HyeongSeok-Choi <154064687+HyeongSeok-Choi@users.noreply.github.com>
2024-07-01 23:10:25 +0900

    20240701

--- src/main/java/com/mes/yangyaggogu/service/productPlanService.java
+++ src/main/java/com/mes/yangyaggogu/service/productPlanService.java
@@ -54,56 +54,56 @@ public class productPlanService {
 
        List<productPlan> BeforeOrderList = P_planRepository.findAllByState(productionPlan_state.beforeOrder);
 
-       List<productPlan> FilterList = new ArrayList<>();
+//       List<productPlan> FilterList = new ArrayList<>();
+//
+//       LocalDate lowStockRegisterDate=null;
+//
+//        for (productPlan productPlan : BeforeOrderList) {
+//
+//            String[] orderNum =  productPlan.getProductionPlanCode().split(",");
+//
+//            System.out.println(orderNum[0].substring(0,orderNum[0].length() - 2)+"매그네릭");
+//            obtainorder_number findObtainNum =obtainorder_NumberRepository.findById(orderNum[0].substring(0,orderNum[0].length() - 2)).orElseThrow();
+//
+//            List<obtainorder_detail> obtainorderDetailList = obtainorder_detailRepository.findByOrderNumber(findObtainNum);
+//
+//
+//            LocalDate deliveryDate1 = null;
+//            for (obtainorder_detail obtainorderDetail:obtainorderDetailList){
+//                deliveryDate1= obtainorderDetail.getDelivery_Date();
+//            }
+//
+//
+//            LocalDate deliveryDate = deliveryDate1;
+//
+//
+//            if (productPlan.getMaterialsName().equals("양배추즙") || productPlan.getMaterialsName().equals("흑마늘즙")) {
+//                lowStockRegisterDate = deliveryDate.minusDays(8);
+//                System.out.println(lowStockRegisterDate);
+//
+//
+//                if (lowStockRegisterDate.getDayOfWeek().getValue() == 6 || lowStockRegisterDate.getDayOfWeek().getValue() == 7) {
+//                    lowStockRegisterDate = lowStockRegisterDate.minusDays(2);
+//                }
+//
+//            } else {
+//                lowStockRegisterDate = deliveryDate.minusDays(9);
+//                System.out.println(lowStockRegisterDate);
+//
+//                if (lowStockRegisterDate.getDayOfWeek().getValue() == 6 || lowStockRegisterDate.getDayOfWeek().getValue() == 7) {
+//                    lowStockRegisterDate = lowStockRegisterDate.minusDays(2);
+//                }
+//
+//            }
+//
+//            if(lowStockRegisterDate.isEqual(LocalDate.now())){
+//
+//                FilterList.add(productPlan);
+//            }
+//        }
 
-       LocalDate lowStockRegisterDate=null;
 
-        for (productPlan productPlan : BeforeOrderList) {
-
-            String[] orderNum =  productPlan.getProductionPlanCode().split(",");
-
-            System.out.println(orderNum[0].substring(0,orderNum[0].length() - 2)+"매그네릭");
-            obtainorder_number findObtainNum =obtainorder_NumberRepository.findById(orderNum[0].substring(0,orderNum[0].length() - 2)).orElseThrow();
-
-            List<obtainorder_detail> obtainorderDetailList = obtainorder_detailRepository.findByOrderNumber(findObtainNum);
-
-
-            LocalDate deliveryDate1 = null;
-            for (obtainorder_detail obtainorderDetail:obtainorderDetailList){
-                deliveryDate1= obtainorderDetail.getDelivery_Date();
-            }
-
-
-            LocalDate deliveryDate = deliveryDate1;
-
-
-            if (productPlan.getMaterialsName().equals("양배추즙") || productPlan.getMaterialsName().equals("흑마늘즙")) {
-                lowStockRegisterDate = deliveryDate.minusDays(8);
-                System.out.println(lowStockRegisterDate);
-
-
-                if (lowStockRegisterDate.getDayOfWeek().getValue() == 6 || lowStockRegisterDate.getDayOfWeek().getValue() == 7) {
-                    lowStockRegisterDate = lowStockRegisterDate.minusDays(2);
-                }
-
-            } else {
-                lowStockRegisterDate = deliveryDate.minusDays(9);
-                System.out.println(lowStockRegisterDate);
-
-                if (lowStockRegisterDate.getDayOfWeek().getValue() == 6 || lowStockRegisterDate.getDayOfWeek().getValue() == 7) {
-                    lowStockRegisterDate = lowStockRegisterDate.minusDays(2);
-                }
-
-            }
-
-            if(lowStockRegisterDate.isEqual(LocalDate.now())){
-
-                FilterList.add(productPlan);
-            }
-        }
-
-
-        return  FilterList;
+        return  BeforeOrderList;
     }
     
     //저장
